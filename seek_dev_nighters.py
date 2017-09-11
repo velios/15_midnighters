2e6b8ae8c986b2305ebcc0ef1c162fd4a9ad1d3d
Елисеев Леонид Владимирович <eliseev_lv@etm.corp>
2017-09-11 14:06:22 +0300

    Add pytz and pendulum solution

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 from collections import namedtuple
-from itertools import chain
 
 import pytz
+import pendulum
 import requests
 
 
 def load_attempt_json_data(page_number=1):
-    api_load_attemts_url = 'https://devman.org/api/challenges/solution_attempts/'
-    r = requests.get(api_load_attemts_url,
-                     params={'page': page_number})
-    return r.json()
+    api_load_attempts_url = 'https://devman.org/api/challenges/solution_attempts/'
+    request = requests.get(api_load_attempts_url,
+                           params={'page': page_number})
+    return request.json()
 
 
 def transform_localize_date_from_timestamp(timestamp, timezone):
@@ -19,42 +19,50 @@ def transform_localize_date_from_timestamp(timestamp, timezone):
     return pytz.timezone(timezone).fromutc(datetime.utcfromtimestamp(timestamp))
 
 
+def transform_localize_date_from_timestamp_pendulum(timestamp, timezone):
+    return pendulum.from_timestamp(timestamp=timestamp,
+                                   tz=timezone)
+
+
 def get_attempts_generator():
-    pages = load_attempt_json_data()['number_of_pages']
-    return [chain(load_attempt_json_data(4)['records']) for x in range(10)]
+    pages_count = load_attempt_json_data()['number_of_pages']
+    for page in range(1, pages_count + 1):
+        for user_data in load_attempt_json_data(page)['records']:
+            yield {
+                'username': user_data['username'],
+                'timestamp': user_data['timestamp'],
+                'timezone': user_data['timezone']
+            }
 
-    # for user_data in chain(load_attempt_json_data(page)['records'] for page in range(pages)):
-    #     yield {
-    #         'username': user_data['username'],
-    #         'timestamp': user_data['timestamp'],
-    #         'timezone': user_data['timezone'],
-    #     }
 
-
-def get_midnighters_list(user_attempts_generator, midnight_start_hour=0, midnight_end_hour=23):
+def get_midnighters_list(user_attempts_generator, midnight_start_hour=0, midnight_end_hour=6):
     midnighters_list = []
     AttemptUserData = namedtuple('AttemptUserData', ['username', 'localized_datetime'])
     for user_attempt_data in user_attempts_generator:
         try:
             attempt_datetime = transform_localize_date_from_timestamp(timestamp=user_attempt_data['timestamp'],
                                                                       timezone=user_attempt_data['timezone'])
+            attempt_datetime2 = transform_localize_date_from_timestamp_pendulum(timestamp=user_attempt_data['timestamp'],
+                                                                                timezone=user_attempt_data['timezone'])
             if midnight_start_hour <= attempt_datetime.hour <= midnight_end_hour:
-                midnighters_list.append(AttemptUserData(username=user_attempt_data['username'],
-                                                        localized_datetime=attempt_datetime))
+                # midnighters_list.append(AttemptUserData(username=user_attempt_data['username'],
+                #                                         localized_datetime=attempt_datetime))
+                midnighters_list.append((user_attempt_data['username'],
+                                         user_attempt_data['timestamp'],
+                                         user_attempt_data['timezone'],
+                                         attempt_datetime,
+                                         attempt_datetime2))
         except:
             pass
     return midnighters_list
 
 
-
-
 if __name__ == '__main__':
     # t = transform_localize_date_from_timestamp(1504080907, 'Asia/Vladivostok')
     # # try:
-    #     # a = get_midnighters_list(user_attempts_generator=get_attempts_generator())
-    a = get_attempts_generator()
-    for i in a:
-        print(i)
+    # #     # a = get_midnighters_list(user_attempts_generator=get_attempts_generator())
+    # a = get_attempts_generator()
+    # print(a)
     # for x, i in enumerate(a):
     #     print('{}=>{}=>{}=>{}'.format(x, i['username'], i['timestamp'], i['timezone']))
     # except TypeError as error:
@@ -62,3 +70,6 @@ if __name__ == '__main__':
     # except pytz.exceptions.UnknownTimeZoneError as error:
     #     print('Timezone error: value {} set to timezone is incorrect'.format(error))
 
+
+    for i in get_midnighters_list(get_attempts_generator()):
+        print(i)
